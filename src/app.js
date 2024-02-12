cbedeb83fc9ae9c09b1102622c263ed3cda210f0
john-kick <andre010400@web.de>
2024-02-12 18:01:33 +0000

    Extracts routes to separate file

--- src/app.js
+++ src/app.js
@@ -1,37 +1,11 @@
 const express = require("express");
-const path = require("path");
+const routes = require("./routes");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get("/audio/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "audio", req.params[0]));
-});
-
-app.get("/soundConfig/*", (req, res) => {
-  const filePath = path.join(__dirname, "..", "soundConfig", req.params[0]);
-  res.set("Content-Type", "application/json");
-  res.sendFile(filePath);
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
-});
-
-app.get("/styles.css", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "styles.css"));
-});
-
-app.get("/dist/index.js", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "dist", "index.js"));
-});
-
-app.get("/dist/*", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "..", "public", "dist", req.params[0] + ".js")
-  );
-});
+app.use("/hinweissystem", routes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
---
+++ src/routes.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+
+const router = express.Router();
+
+router.get("/audio/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "audio", req.params[0]));
+});
+
+router.get("/soundConfig/*", (req, res) => {
+  const filePath = path.join(__dirname, "..", "soundConfig", req.params[0]);
+  res.set("Content-Type", "application/json");
+  res.sendFile(filePath);
+});
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
+
+router.get("/styles.css", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "styles.css"));
+});
+
+router.get("/dist/index.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "dist", "index.js"));
+});
+
+router.get("/dist/*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "..", "public", "dist", req.params[0] + ".js")
+  );
+});
+
+module.exports = router;
